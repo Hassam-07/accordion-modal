bf54176d63ef53608cca6abc7637b8a0aeed2fbe
Hassam <101381873+Hassam-07@users.noreply.github.com>
2024-01-29 14:29:42 +0500

    accordion modal completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'accordion-modal';
+  // isContentExpanded = false;
+  isContentExpanded: boolean[] = [false, false, false, false];
+  accordion(index: number) {
+    // this.isContentExpanded = !this.isContentExpanded;
+    this.isContentExpanded[index] = !this.isContentExpanded[index];
+  }
 }
